ffeaa93813ef1f6c0d8821295796f614681f5732
gsatish100 <gsatish100@gmail.com>
2014-07-18 16:59:43 +0530

    Create boardport-files.c
    board port files for am335x-evm

---
+++ boardport-files.c
@@ -0,0 +1,9 @@
+omap board port files for am335x-evm 
+
+arch/arm/configs/am335x_evm_defconfig
+arch/arm/mach-omap2/board-am335xevm.c
+arch/arm/mach-omap2/devices.c
+arch/arm/mach-omap2/devices.h
+arch/arm/mach-omap2/id.c
+arch/arm/mach-omap2/include/mach/board-am335xevm.h
+
