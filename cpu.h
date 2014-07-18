56f1547ee6c479c5c01b35bc27901b9a8043ced8
gsatish100 <gsatish100@gmail.com>
2014-07-18 18:13:08 +0530

    Create cpu.h
    OMAP cpu type detection

---
+++ cpu.h
@@ -0,0 +1,9 @@
+File Path: linnux/arch/arm/plat-omap/include/mach/cpu.h
+File Name: cpu.h
+
+
+
+#define AM335X_CLASS		0x33500034
+ #define AM335X_REV_ES1_0	AM335X_CLASS
+ #define AM335X_REV_ES2_0	(AM335X_CLASS | (0x1 << 8))
++#define AM335X_REV_ES2_1	(AM335X_CLASS | (0x2 << 8))
