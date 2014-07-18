5525aacdeeae9b4d16b153b8541a6c5ed63d697f
gsatish100 <gsatish100@gmail.com>
2014-07-18 18:26:58 +0530

    Update boardport-files.c

--- boardport-files.c
+++ boardport-files.c
@@ -6,4 +6,11 @@ arch/arm/mach-omap2/devices.c
 arch/arm/mach-omap2/devices.h
 arch/arm/mach-omap2/id.c
 arch/arm/mach-omap2/include/mach/board-am335xevm.h
+arch/arm/mach-omap2/opp3xxx_data.c
+arch/arm/mach-omap2/pm33xx.c
+arch/arm/mach-omap2/timer.c
+arch/arm/plat-omap/dmtimer.c
+arch/arm/plat-omap/include/plat/cpu.h
+arch/arm/plat-omap/include/plat/dmtimer.h
+
 
