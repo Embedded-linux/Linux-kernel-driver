4a07f6d1b796e22a71fc65b75a0a6a582c3fa7d9
gsatish100 <gsatish100@gmail.com>
2014-07-20 16:51:37 +0530

    Create cppi41.h
    CPPI 4.1 definitions

---
+++ cppi41.h
@@ -0,0 +1,14 @@
+File path: drivers/usb/musb/cppi41.h
+File name: cppi41.h
+
+ CPPI 4.1 definitions
+ 
+ 
+ * DMA Scheduler - Table Region
+  */
+ #define DMA_SCHED_TABLE_WORD_REG(n)	((n) << 2)
+-#define MAX_SCHED_TBL_ENTRY     8
++#define MAX_SCHED_TBL_ENTRY     60
+
+
+
